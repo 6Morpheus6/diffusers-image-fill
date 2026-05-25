137c18100009c96f1179eea5e187301d17e4c7fb
6Morpheus6 <wanton-decay@gmx.net>
2026-05-25 03:55:29 +0200

    update
    pin numpy for linux nvidia

--- torch.js
+++ torch.js
@@ -51,9 +51,12 @@ module.exports = {
         "bluefairy": "off",
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall"
-      },
-      "next": null
+        "message": [
+          "uv pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 {{args && args.xformers ? 'xformers==0.0.30' : ''}} --index-url https://download.pytorch.org/whl/cu128 --force-reinstall",
+          "uv pip install numpy==1.26.4"
+        ],
+        "next": null
+      }
     },
     // amd linux (rocm)
     {
